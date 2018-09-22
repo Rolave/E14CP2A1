f2038abf10e9e7f2db4b83c7657de8f833fe0176
Rodrigo Olave Carrasco <rholave@gmail.com>
2018-09-22 15:49:13 -0300

    fix: corregir delete

--- company_app/app/controllers/companies_controller.rb
+++ company_app/app/controllers/companies_controller.rb
@@ -54,6 +54,7 @@ class CompaniesController < ApplicationController
   # DELETE /companies/1
   # DELETE /companies/1.json
   def destroy
+    @company.employees.destroy_all
     @company.destroy
     respond_to do |format|
       format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
